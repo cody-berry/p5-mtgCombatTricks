f7d9cfc63c8424b465623f6cec53a52e95827ff1
Cody <cody.tian@icloud.com>
2023-01-14 15:20:16 -0800

    fill in author and date

--- sketch.js
+++ sketch.js
@@ -1,16 +1,20 @@
 /**
- *  @author 
- *  @date 2022.05.
+ *  @author Cody
+ *  @date 2023.01.20
  *
  */
 
 let font
+let fixedWidthFont
+let variableWidthFont
 let instructions
 let debugCorner /* output debug text in the bottom left corner of the canvas */
 
 
 function preload() {
     font = loadFont('data/consola.ttf')
+    fixedWidthFont = loadFont('data/consola.ttf')
+    variableWidthFont = loadFont('data/meiryo.ttf')
 }
 
 
@@ -32,11 +36,10 @@ function setup() {
 function draw() {
     background(234, 34, 24)
 
-
     /* debugCorner needs to be last so its z-index is highest */
     debugCorner.setText(`frameCount: ${frameCount}`, 2)
     debugCorner.setText(`fps: ${frameRate().toFixed(0)}`, 1)
-    debugCorner.show()
+    debugCorner.showBottom()
 
     if (frameCount > 3000)
         noLoop()
@@ -50,12 +53,18 @@ function keyPressed() {
         instructions.html(`<pre>
             sketch stopped</pre>`)
     }
+
+    if (key === '`') { /* toggle debug corner visibility */
+        debugCorner.visible = !debugCorner.visible
+        console.log(`debugCorner visibility set to ${debugCorner.visible}`)
+    }
 }
 
 
 /** 🧹 shows debugging info using text() 🧹 */
 class CanvasDebugCorner {
     constructor(lines) {
+        this.visible = true
         this.size = lines
         this.debugMsgList = [] /* initialize all elements to empty string */
         for (let i in lines)
@@ -68,20 +77,69 @@ class CanvasDebugCorner {
         } else this.debugMsgList[index] = text
     }
 
-    show() {
-        textFont(font, 14)
-        strokeWeight(1)
+    showBottom() {
+        if (this.visible) {
+            noStroke()
+            textFont(fixedWidthFont, 14)
 
-        const LEFT_MARGIN = 10
-        const DEBUG_Y_OFFSET = height - 10 /* floor of debug corner */
-        const LINE_SPACING = 2
-        const LINE_HEIGHT = textAscent() + textDescent() + LINE_SPACING
-        fill(0, 0, 100, 100) /* white */
-        strokeWeight(0)
+            const LEFT_MARGIN = 10
+            const DEBUG_Y_OFFSET = height - 10 /* floor of debug corner */
+            const LINE_SPACING = 2
+            const LINE_HEIGHT = textAscent() + textDescent() + LINE_SPACING
 
-        for (let index in this.debugMsgList) {
-            const msg = this.debugMsgList[index]
-            text(msg, LEFT_MARGIN, DEBUG_Y_OFFSET - LINE_HEIGHT * index)
+            /* semi-transparent background */
+            fill(0, 0, 0, 10)
+            rectMode(CORNERS)
+            const TOP_PADDING = 3 /* extra padding on top of the 1st line */
+            rect(
+                0,
+                height,
+                width,
+                DEBUG_Y_OFFSET - LINE_HEIGHT * this.debugMsgList.length - TOP_PADDING
+            )
+
+            fill(0, 0, 100, 100) /* white */
+            strokeWeight(0)
+
+            for (let index in this.debugMsgList) {
+                const msg = this.debugMsgList[index]
+                text(msg, LEFT_MARGIN, DEBUG_Y_OFFSET - LINE_HEIGHT * index)
+            }
+        }
+    }
+
+    showTop() {
+        if (this.visible) {
+            noStroke()
+            textFont(fixedWidthFont, 14)
+
+            const LEFT_MARGIN = 10
+            const TOP_PADDING = 3 /* extra padding on top of the 1st line */
+
+            /* offset from top of canvas */
+            const DEBUG_Y_OFFSET = textAscent() + TOP_PADDING
+            const LINE_SPACING = 2
+            const LINE_HEIGHT = textAscent() + textDescent() + LINE_SPACING
+
+            /* semi-transparent background, a console-like feel */
+            fill(0, 0, 0, 10)
+            rectMode(CORNERS)
+
+            rect( /* x, y, w, h */
+                0,
+                0,
+                width,
+                DEBUG_Y_OFFSET + LINE_HEIGHT*this.debugMsgList.length/*-TOP_PADDING*/
+            )
+
+            fill(0, 0, 100, 100) /* white */
+            strokeWeight(0)
+
+            textAlign(LEFT)
+            for (let i in this.debugMsgList) {
+                const msg = this.debugMsgList[i]
+                text(msg, LEFT_MARGIN, LINE_HEIGHT*i + DEBUG_Y_OFFSET)
+            }
         }
     }
 }
